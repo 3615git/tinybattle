89ebf4a0574e5f9f44204055b262dd06d1bb2629
Rémi <rtheys@splio.com>
2020-05-26 21:55:47 +0200

    Adding Jarvis

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,6 +24,7 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
+const Jarvis = require("webpack-jarvis");
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -656,6 +657,9 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      new Jarvis({
+        port: 1338 // optional: set a port
+      }),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
